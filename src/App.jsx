1dde05948e4d1c03e8594aca17105630299d9041
MariusAskelovic <72157256+MariusAskelovic@users.noreply.github.com>
2023-08-16 14:12:57 +0300

    Features2

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import Cta from './components/CTA/CTA';
 import Features from './components/Features/Features';
+import Features2 from './components/Features2/Features2';
 import Btn from './components/UI/btn/Btn';
 import GainMore from './components/gainMoreSection/GainMore';
 
@@ -11,6 +12,7 @@ export default function App() {
       <Cta />
       <GainMore />
       <Features />
+      <Features2 />
     </div>
   );
 }
---
+++ src/components/Features2/Features2.jsx
@@ -0,0 +1,25 @@
+import Btn from "../UI/btn/Btn";
+import SectionTitle from "../UI/sectionTitle/SectionTitle";
+import css from './Features2.module.css'
+
+export default function Features2() {
+    return (
+        <div className="container">
+            <div className={css.grid}>
+                <div className={css.left}>
+                    <SectionTitle
+                        left
+                        light
+                        big
+                        bigSub
+                        pill='features'
+                        title='Gain more insight into how people use your'
+                        subtitle='With our integrated CRM, project management, collaboration and invoicing capabilities, you can manage every aspect of your business in one secure platform.'
+                    />
+                    <Btn>Get Started</Btn>
+                </div>
+                <img src="/img/gainMore.jpg" alt="gain more" />
+            </div>
+        </div>
+    )
+}
